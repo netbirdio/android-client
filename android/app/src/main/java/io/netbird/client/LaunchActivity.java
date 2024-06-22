bb667e9cc22d099db44bb29f88875ee77e85ecdf
mineiwik <29404520+mineiwik@users.noreply.github.com>
2024-06-22 21:33:18 +0000

    Fix issue with in-app browser closing on app re-open

---
+++ android/app/src/main/java/io/netbird/client/LaunchActivity.java
@@ -0,0 +1,19 @@
+package io.netbird.client;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class LaunchActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        MainApplication application = (MainApplication) getApplication();
+        // check that MainActivity is not started yet
+        if (!application.isActivityInBackStack(MainActivity.class)) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+        finish();
+    }
+}
