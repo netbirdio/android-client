284c304ff54ab938d89946b354d67f566b1a31bb
Zoltán Papp <zoltan.pmail@gmail.com>
2025-04-28 11:52:40 +0200

    Remove unused UI elements

--- app/src/main/java/io/netbird/client/MainActivity.java
+++ app/src/main/java/io/netbird/client/MainActivity.java
@@ -29,14 +29,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.appBarMain.toolbar);
-        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null)
-                        .setAnchorView(R.id.fab).show();
-            }
-        });
+
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each
