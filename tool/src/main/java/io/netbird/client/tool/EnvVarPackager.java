d7175d7a53bdd80401a818e9737fce3d4474d9f5
Diego Romar <diego@diego.local>
2025-09-03 18:38:34 -0300

    Add packager for environment variables

---
+++ tool/src/main/java/io/netbird/client/tool/EnvVarPackager.java
@@ -0,0 +1,14 @@
+package io.netbird.client.tool;
+
+import io.netbird.gomobile.android.Android;
+import io.netbird.gomobile.android.EnvList;
+
+public class EnvVarPackager {
+    public static EnvList getEnvironmentVariables(Preferences preferences) {
+        var envList = new EnvList();
+
+        envList.put(Android.getEnvKeyNBForceRelay(), String.valueOf(preferences.isRelayConnectionEnforced()));
+
+        return envList;
+    }
+}
