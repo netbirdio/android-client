b6f1bc84ee715f9db582d9dd5453dff6dc5f9b2a
Berkant İpek <41230766+berkant@users.noreply.github.com>
2025-09-12 05:41:31 +0300

    Update EnvVarPackager.java

--- tool/src/main/java/io/netbird/client/tool/EnvVarPackager.java
+++ tool/src/main/java/io/netbird/client/tool/EnvVarPackager.java
@@ -8,6 +8,10 @@ public class EnvVarPackager {
         var envList = new EnvList();
 
         envList.put(Android.getEnvKeyNBForceRelay(), String.valueOf(preferences.isConnectionForceRelayed()));
+        envList.put("NB_ICE_DISCONNECTED_TIMEOUT_SEC", "25");
+        envList.put("NB_ICE_FAILED_TIMEOUT_SEC", "25");
+        envList.put("NB_ICE_FORCE_RELAY_CONN", "true");
+        // envList.put("NB_LOG_LEVEL", "trace");
 
         return envList;
     }
