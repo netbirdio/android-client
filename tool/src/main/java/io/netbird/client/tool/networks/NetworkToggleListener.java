806677988bf5f4574c761804bc0213ef8b9115d9
Diego Romar <18450339+doromaraujo@users.noreply.github.com>
2025-10-16 21:28:43 -0300

    Add some documentation to NetworkToggleListener

--- tool/src/main/java/io/netbird/client/tool/networks/NetworkToggleListener.java
+++ tool/src/main/java/io/netbird/client/tool/networks/NetworkToggleListener.java
@@ -1,5 +1,9 @@
 package io.netbird.client.tool.networks;
 
+/**
+ * NetworkToggleListener defines a callback to be fired when
+ * the device's currently available network type changes.
+ */
 public interface NetworkToggleListener {
     void onNetworkTypeChanged();
 }
