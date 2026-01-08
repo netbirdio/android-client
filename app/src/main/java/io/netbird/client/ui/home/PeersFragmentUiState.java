e6e00911ad22d9ba910e3835f402f748084f2bc0
Diego Romar <18450339+doromaraujo@users.noreply.github.com>
2026-01-08 15:12:18 -0300

    Make UI state immutable

--- app/src/main/java/io/netbird/client/ui/home/PeersFragmentUiState.java
+++ app/src/main/java/io/netbird/client/ui/home/PeersFragmentUiState.java
@@ -1,15 +1,17 @@
 package io.netbird.client.ui.home;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class PeersFragmentUiState {
     private final List<Peer> peers;
 
     public PeersFragmentUiState(List<Peer> peers) {
-        this.peers = peers;
+        this.peers = new ArrayList<>(peers);
     }
 
     public List<Peer> getPeers() {
-        return peers;
+        return Collections.unmodifiableList(peers);
     }
 }
